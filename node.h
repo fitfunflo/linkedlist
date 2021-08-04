f8987be3967203c4326b614fe8401277a1a322a1
FITFUNFLO <7it7un7lo@gmail.com>
2021-08-04 14:49:51 -0400

    Update node.h

--- node.h
+++ node.h
@@ -1,7 +1,22 @@
+typedef struct BookData {
+  unsigned_int id_book;
+  unsigned int id_data;
+} BookData;
+
+typedef struct SongData {
+  unsigned int id_song;
+  unsigned int id_data;
+} SongData;
+
+typedef struct Data {
+  unsigned int id;
+  unsigned int type;
+} Data;
+
 typedef struct Node {
-  void * data;
+  Data data;
   struct Node * next;
-  struct Node * prev;
+  struct Node * previous;
 } Node;
 
 void node_build(void * data_array, size_t data_size, Node * head, unsigned int size) {
@@ -11,8 +26,8 @@ void node_build(void * data_array, size_t data_size, Node * head, unsigned int s
     Node * addr = head + i;
     *addr = node;
     previous = addr;
-    if (node.prev) {
-      (node.prev)->next = addr;
+    if (node.previous) {
+      (node.previous)->next = addr;
     }
   }
 }
